4c79970066d30a2726e54bb8748981a674ff04e8
Ben Cason <bencason9@gmail.com>
2019-07-25 18:41:32 -0400

    removed readme

--- clickygame/src/components/Nav.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Nav() {
-  return (
-    <nav className="navbar navbar-primary bg-primary" style={{fontSize: 30, color: "white"}}>Clicky Game!</nav>
-  );
-}
-
-export default Nav;
